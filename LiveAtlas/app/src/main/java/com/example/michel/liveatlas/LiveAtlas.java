99777b75352144b539fc936b00f2df9d22f525c8
michelyjiang@hotmail.com <michelyjiang@hotmail.com>
2016-04-22 21:49:34 -0400

    first ever commit

---
+++ LiveAtlas/app/src/main/java/com/example/michel/liveatlas/LiveAtlas.java
@@ -0,0 +1,14 @@
+package com.example.michel.liveatlas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LiveAtlas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_atlas);
+    }
+}
+us
